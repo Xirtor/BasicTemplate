84646563c9d2960948c2bc10489e63f854064daa
Egor Vasyakin <evasyakin@mail.ru>
2017-04-18 05:04:56 +0700

    Micro application
    Micro, Router, Route and simple examples

--- index.php
+++ index.php
@@ -1,14 +1,32 @@
 <?php
 
-// Вывод ошибок
+// Error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// Автозагрузчик классов
+// Autoload
 require 'vendor/Xirtor/Loader.php';
 $loader = new Xirtor\Loader;
 $loader->register();
 
-// Менеджер журнала
+// Logger
 $logger = new Xirtor\Logger('log.txt');
 $logger->write('hello');
+
+
+// Micro application example
+
+
+// create application
+$app = new Xirtor\Web\Micro;
+
+// callback handler
+$app->get('/', function (){
+	echo 'Home page';
+});
+
+// handler in file
+$app->get('/auth', 'app/handlers/auth');
+$app->get('/registration', 'app/handlers/registration');
+
+$app->handle();
