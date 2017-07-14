6eb4793565387c0195005fdc6fb5aeb06737afa0
Egor Vasyakin <evasyakin@mail.ru>
2017-07-14 20:17:30 +0700

    Revert "Xirtor\Loader"
    This reverts commit a1e0f75b8e779e178b5ac91bc572c75cf28e15e5.

--- vendor/Xirtor/Loader.php
+++ vendor/Xirtor/Loader.php
@@ -1,47 +1,51 @@
 <?php
 /**
 * @package Xirtor
-* @link https://github.com/xirtor
-* @copyright Copyright (c) XirtorTeam
+* @link http://github.com/Xirtor/
+* @copyright Copyright (c) Xirtor
 */
-
 namespace Xirtor;
-
-use Xirtor\Exception;
 
 /**
 * Classes autoloader
-* @author Egor Vasyakin <egor.vasykin@itevas.ru>
-* @since 1.0
+* Support standarts PSR4, PSR0
+* @since 0.1 Beta
+* @author Egor Vasyakin <egor.vasyakin@itevas.ru>
 */
 
 class Loader{
 
-	public $classes = [];
-	public $namespaces = [];
-	public $dirs = [];
-	public $registered = false;
+	protected $_classes = [];
+	protected $_namespaces = [];
+	protected $_dirs = [];
+	protected $_registered = false;
 
 	public function __construct(){
 		$this->registerDir('vendor/');
 	}
 
 	public function registerClass($name, $path){
-		$this->classes[$name] = $path;
+		$this->_classes[$name] = $path;
+	}
+
+	public function registerClasses(array $classes){
+		foreach ($classes as $name => $path) {
+			$this->registerClass($name, $path);
+		}
 	}
 
 	public function registerNamespace($name, $path){
-		$this->namespaces[$name] = $path;
+		$this->_namespaces[$name] = $path;
 	}
 
 	public function registerNamespaces(array $namespaces){
-		foreach ($namespaces as $name => $path) {
-			$this->registerNamespace($name, $path);
+		foreach ($namespaces as $namespace => $path) {
+			$this->registerNamespace($namespace, $path);
 		}
 	}
 
 	public function registerDir($dir){
-		$this->dirs[] = $dir;
+		$this->_dirs[] = $dir;
 	}
 
 	public function registerDirs(array $dirs){
@@ -51,33 +55,49 @@ class Loader{
 	}
 
 	public function register(){
-		spl_autoload_register([$this, 'autoload']);
-		$this->registered = true;
+		if ($this->_registered === false) {
+			spl_autoload_register([$this, 'autoload']);
+			$this->_registered = true;
+		}
 	}
 
 	public function unregister(){
 		spl_autoload_unregister([$this, 'autoload']);
-		$this->registered = false;
+		$this->_registered = false;
 	}
 
 	public function autoload($className){
-		$className = str_replace('\\', '/', $className);
+		$filename = isset($this->_classes[$className]) ? $this->_classes[$className] : false;
+		if (!is_readable($filename)) $filename = false;
 
-		if (isset($this->classes[$className])) {
-			$filename = $this->classes[$className];
-			if (is_readable($filename)) return include $filename;
-		}
-		foreach ($this->namespaces as $name => $path) {
-			if (strpos($className, $name) === 0) {
-				$filename = $path . $className . '.php';
-				if (is_readable($filename)) return include $filename;
+
+		if (!$filename) {
+
+			foreach ($this->_namespaces as $name => $path) {
+				if (preg_match("/^$name/", $className)) {
+					$tmp = $path . $className . '.php';
+					if (is_readable($tmp)) {
+						$filename = $tmp;
+						break;
+					}
+				}
 			}
+
+			if (!$filename) {
+
+				foreach ($this->_dirs as $path) {
+					$tmp = $path . $className . '.php';
+					if (is_readable($tmp)) {
+						$filename = $tmp;
+						break;
+					}
+				}
+
+			}
+
 		}
-		foreach ($this->dirs as $dir) {
-			$filename = $dir . $className . '.php';
-			if (is_readable($filename)) return include $filename;
-		}
-		throw new Exception('Could not found class "' . str_replace('/', '\\', $className) . '"');
+
+		if ($filename) require $filename;
 	}
 
 }
