8429c2a9e1fdca74635b1bf2a654747a9afdf66e
Egor Vasyakin <evasyakin@mail.ru>
2017-07-14 20:21:00 +0700

    Базовый объект и DI контейнер
    Xirtor\Object
    Xirtor\Di

---
+++ vendor/Xirtor/Di.php
@@ -0,0 +1,32 @@
+<?php
+/**
+* @package Xirtor
+* @link https://github.com/xirtor
+* @copyright Copyright (c) XirtorTeam
+*/
+
+namespace Xirtor;
+
+use Xirtor\Object;
+
+/**
+* Di container
+* @author Egor Vasyakin <egor.vasykin@itevas.ru>
+* @since 1.0
+*/
+
+class Di extends Object{
+	
+	public function __set($name, $value){
+		$this->$name = $value instanceof Closure ? $value->bindTo($this) : $value;
+	}
+
+	public function __get($name){
+		return isset($this->$name) ? $this->$name : null;
+	}
+
+	public function __call($name, array $arguments = null){
+		return is_callable($this->$name) ? call_user_func_array($this->$name, $arguments) : null;
+	}
+
+}
---
+++ vendor/Xirtor/Object.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* @package Xirtor
+* @link https://github.com/xirtor
+* @copyright Copyright (c) XirtorTeam
+*/
+
+namespace Xirtor;
+
+/**
+* Base object class
+* @author Egor Vasyakin <egor.vasykin@itevas.ru>
+* @since 1.0
+*/
+
+class Object{
+
+	public static function className(){
+		return get_called_class();
+	}
+	
+	public function __construct(array $config = null){
+		if ($config) foreach ($config as $name => $value) $this->$name = $value;
+		$this->init();
+	}
+
+	public function init(){
+		// 
+	}
+
+	public function __set($name, $value){
+		// 
+	}
+
+	public function __get($name){
+		// 
+	}
+
+}
