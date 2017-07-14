d1e45d9e8b3381009f9be89b04e2c7f9c45f214a
Egor Vasyakin <evasyakin@mail.ru>
2017-07-14 20:31:23 +0700

    Index file in Basic Template
    автозагрузка классов
    создание приложения
    устанавка маршрутизатора
    установка класса представлений
    запуск приложения

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+// вывод ошибок
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+try {
+
+	// автозагрузка классов
+	include 'vendor/Xirtor/Loader.php';
+	$loader = new Xirtor\Loader;
+	$loader->registerDir('app/models/');
+	$loader->register();
+
+	// создание приложения
+	$app = new Xirtor\Web\App;
+
+	// устанавка маршрутизатора
+	$app->setRouter();
+
+	// установка класса представлений
+	$app->setView();
+
+	// запуск приложения
+	$app->handle();
+
+} catch (Exception $e) {
+	echo 'Xirtor catch exception: ' . $e->getMessage();
+}
