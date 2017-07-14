d32ad9baf370ddc4dee2275183ae2e2f179a2fde
Egor Vasyakin <evasyakin@mail.ru>
2017-07-14 20:19:54 +0700

    Исключения
    Xirtor\Exception

---
+++ vendor/Xirtor/Exception.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* @package Xirtor
+* @link https://github.com/xirtor
+* @copyright Copyright (c) XirtorTeam
+*/
+
+namespace Xirtor;
+
+/**
+* Exception class
+* @author Egor Vasyakin <egor.vasykin@itevas.ru>
+* @since 1.0
+*/
+
+class Exception extends \Exception{
+	
+	public function __construct($message, $code = 0, Exception $previous = null){
+		// die ('Xirtor catch exception: ' . $message);
+		// echo $message = 'Xirtor catch exception: ' . $message;
+		parent::__construct($message, $code, $previous);
+	}
+
+}
